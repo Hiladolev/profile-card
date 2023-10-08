5e7b27530eb8af874c2d5a98f78e4bcf4316ce7d
Hila <hiladolev1@gmail.com>
2023-10-08 19:55:17 +0300

    className for img css declarations

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,3 +1,9 @@
 export default function Avatar() {
-  return <img src="images/selfie.jfif" alt="selfie of Hila Dolev" />;
+  return (
+    <img
+      src="images/selfie.jfif"
+      alt="selfie of Hila Dolev"
+      className="avatar"
+    />
+  );
 }

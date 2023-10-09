d84c16e23679d3fdde8f7b49fc4ae6861acd916f
Hila <hiladolev1@gmail.com>
2023-10-09 15:39:00 +0300

    intro about myself

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+export default function Intro() {
+  return (
+    <div>
+      <h1>Hila Dolev</h1>
+      <p>
+        Full stack web developer. When not coding, I like doing Crossfit, dance
+        or just to cook something sweet.
+      </p>
+    </div>
+  );
+}

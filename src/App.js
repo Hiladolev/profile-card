fc03e33a836f68b120b1067bab577e1790e4bb9d
Hila <hiladolev1@gmail.com>
2023-10-09 16:53:01 +0300

    skills list array and component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Avatar from "./components/Avatar";
 import "./styles.css";
 import Intro from "./components/Intro";
+import SkillList from "./components/SkillsList/SkillsList";
 
 export default function App() {
+  const skills = [
+    "HTML+CSS",
+    "Javascript",
+    "Git and Github",
+    "React",
+    "NodeJs",
+    "Express",
+    "MySQL",
+  ];
   return (
     <div className="card">
       <Avatar />
       <div className="data">
         <Intro />
+        <SkillList skills={skills} />
       </div>
     </div>
   );

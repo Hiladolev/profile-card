6636761196a6a2fa42d3c87fc5eca66b0e722355
Hila <hiladolev1@gmail.com>
2023-10-09 15:39:37 +0300

    intro component was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Avatar from "./components/Avatar";
 import "./styles.css";
+import Intro from "./components/Intro";
 
 export default function App() {
   return (
     <div className="card">
       <Avatar />
-      <div className="data"></div>
+      <div className="data">
+        <Intro />
+      </div>
     </div>
   );
 }

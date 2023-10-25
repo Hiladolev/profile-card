62e4d072305175f280f7a90ed6711539fae835df
Hila <hiladolev1@gmail.com>
2023-10-25 17:59:25 +0300

    destructuring objects - props

--- src/components/SkillsList/Skill.js
+++ src/components/SkillsList/Skill.js
@@ -1,8 +1,8 @@
-export default function Skill(props) {
+export default function Skill({ skill }) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return (
     <li className="skill" style={{ backgroundColor: `#${randomColor}` }}>
-      {props.skill}
+      {skill}
     </li>
   );
 }

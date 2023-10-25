d2f85af0efc2ea4a245190bfe00fd4e98710e9be
Hila <hiladolev1@gmail.com>
2023-10-25 18:03:11 +0300

    adding skill level

--- src/components/SkillsList/Skill.js
+++ src/components/SkillsList/Skill.js
@@ -1,5 +1,7 @@
-export default function Skill({ skill }) {
+export default function Skill({ skill, level }) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const skillLevel = level;
+
   return (
     <li className="skill" style={{ backgroundColor: `#${randomColor}` }}>
       {skill}
--- src/components/SkillsList/SkillsList.js
+++ src/components/SkillsList/SkillsList.js
@@ -3,7 +3,7 @@ export default function SkillsList({ skills }) {
   return (
     <ul className="skill-list">
       {skills.map((item) => (
-        <Skill skill={item.skill} key={item.skill} />
+        <Skill skill={item.skill} key={item.skill} level={item.level} />
       ))}
     </ul>
   );

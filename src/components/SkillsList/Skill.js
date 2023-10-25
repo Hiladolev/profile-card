da54f1ce26099b9c6c308a27366b1daebc43a41a
Hila <hiladolev1@gmail.com>
2023-10-25 18:12:16 +0300

    switch case for 3 options of emojis by skill level

--- src/components/SkillsList/Skill.js
+++ src/components/SkillsList/Skill.js
@@ -1,10 +1,20 @@
 export default function Skill({ skill, level }) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  const skillLevel = level;
-
+  let emoji = "";
+  switch (level) {
+    case "advanced":
+      emoji = "💪";
+      break;
+    case "intermediate":
+      emoji = "👍";
+      break;
+    default:
+      emoji = "👶";
+  }
   return (
     <li className="skill" style={{ backgroundColor: `#${randomColor}` }}>
       {skill}
+      {emoji}
     </li>
   );
 }

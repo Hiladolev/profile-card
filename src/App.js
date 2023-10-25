6a065e2041d651a5ee66f616a541fb9e6a838de2
Hila <hiladolev1@gmail.com>
2023-10-25 17:13:13 +0300

    changed skills array into array of objects-added level of skill

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import Intro from "./components/Intro";
 import SkillList from "./components/SkillsList/SkillsList";
 
 export default function App() {
+  // const skills = [
+  //   "HTML+CSS💪",
+  //   "Javascript💪",
+  //   "Git and Github👍",
+  //   "React💪",
+  //   "NodeJs👶",
+  //   "Express👶",
+  //   "MySQL💪",
+  // ];
   const skills = [
-    "HTML+CSS💪",
-    "Javascript💪",
-    "Git and Github👍",
-    "React💪",
-    "NodeJs👶",
-    "Express👶",
-    "MySQL💪",
+    { skill: "HTML+CSS", level: "advanced" },
+    { skill: "Javascript", level: "advanced" },
+    { skill: "Git and Github", level: "intermediate" },
+    { skill: "React", level: "advanced" },
+    { skill: "NodeJs", level: "intermediate" },
+    { skill: "Express", level: "intermediate" },
+    { skill: "MySQL", level: "advanced" },
   ];
   return (
     <div className="card">
